bf7edd4a2ca06a4027f39c61b6df1bee16395dce
Sergiu Marton <brown121407@gmail.com>
2018-07-01 22:16:00 +0300

    Adaugat BooleanLiteral

---
+++ PseudocodeInterpreter/src/Objects/BooleanLiteral.cs
@@ -0,0 +1,23 @@
+﻿namespace PseudocodeInterpreter.Objects
+{
+    class BooleanLiteral : Literal
+    {
+		public bool Value { get; }
+
+	    public BooleanLiteral(bool value)
+	    {
+		    Type = TypeNames.Boolean;
+		    Value = value;
+		}
+
+	    public override bool ToBoolean()
+	    {
+		    return Value;
+	    }
+
+	    public override string ToString()
+	    {
+		    return Value.ToString();
+	    }
+    }
+}
