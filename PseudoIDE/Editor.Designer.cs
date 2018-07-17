78484338fc4d615bbac946f46d9522d00fbeb822
Sergiu Marton <alexander.sergiu@gmail.com>
2018-07-17 19:56:28 +0300

    Created Editor (Windows Forms) project

---
+++ PseudoIDE/Editor.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace PseudoIDE
+{
+	partial class Editor
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Editor));
+			this.toolStrip = new System.Windows.Forms.ToolStrip();
+			this.fileMenu = new System.Windows.Forms.ToolStripDropDownButton();
+			this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.runButton = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+			this.statusStrip = new System.Windows.Forms.StatusStrip();
+			this.lineLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.columnLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.fileNameLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStrip.SuspendLayout();
+			this.statusStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// toolStrip
+			// 
+			this.toolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+			this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenu,
+            this.toolStripSeparator,
+            this.runButton});
+			this.toolStrip.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip.Name = "toolStrip";
+			this.toolStrip.Size = new System.Drawing.Size(800, 25);
+			this.toolStrip.TabIndex = 0;
+			// 
+			// fileMenu
+			// 
+			this.fileMenu.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.fileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.fileMenu.Image = ((System.Drawing.Image)(resources.GetObject("fileMenu.Image")));
+			this.fileMenu.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.fileMenu.Name = "fileMenu";
+			this.fileMenu.Size = new System.Drawing.Size(38, 22);
+			this.fileMenu.Text = "&File";
+			// 
+			// newToolStripMenuItem
+			// 
+			this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+			this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.newToolStripMenuItem.Text = "&New";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.openToolStripMenuItem.Text = "&Open";
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.saveToolStripMenuItem.Text = "&Save";
+			// 
+			// saveAsToolStripMenuItem
+			// 
+			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.saveAsToolStripMenuItem.Text = "Save &As";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.exitToolStripMenuItem.Text = "&Exit";
+			// 
+			// runButton
+			// 
+			this.runButton.Image = ((System.Drawing.Image)(resources.GetObject("runButton.Image")));
+			this.runButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.runButton.Name = "runButton";
+			this.runButton.Size = new System.Drawing.Size(48, 22);
+			this.runButton.Text = "&Run";
+			// 
+			// toolStripSeparator
+			// 
+			this.toolStripSeparator.Name = "toolStripSeparator";
+			this.toolStripSeparator.Size = new System.Drawing.Size(6, 25);
+			// 
+			// statusStrip
+			// 
+			this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileNameLabel,
+            this.lineLabel,
+            this.columnLabel});
+			this.statusStrip.Location = new System.Drawing.Point(0, 428);
+			this.statusStrip.Name = "statusStrip";
+			this.statusStrip.Size = new System.Drawing.Size(800, 22);
+			this.statusStrip.TabIndex = 1;
+			this.statusStrip.Text = "statusStrip1";
+			// 
+			// lineLabel
+			// 
+			this.lineLabel.AutoSize = false;
+			this.lineLabel.Name = "lineLabel";
+			this.lineLabel.Size = new System.Drawing.Size(75, 17);
+			this.lineLabel.Text = "Ln";
+			this.lineLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// columnLabel
+			// 
+			this.columnLabel.AutoSize = false;
+			this.columnLabel.Name = "columnLabel";
+			this.columnLabel.Size = new System.Drawing.Size(75, 17);
+			this.columnLabel.Text = "Col";
+			this.columnLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// fileNameLabel
+			// 
+			this.fileNameLabel.Name = "fileNameLabel";
+			this.fileNameLabel.Size = new System.Drawing.Size(635, 17);
+			this.fileNameLabel.Spring = true;
+			this.fileNameLabel.Text = "FileName";
+			this.fileNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// Editor
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.statusStrip);
+			this.Controls.Add(this.toolStrip);
+			this.MinimumSize = new System.Drawing.Size(400, 400);
+			this.Name = "Editor";
+			this.Text = "PseudoIDE";
+			this.toolStrip.ResumeLayout(false);
+			this.toolStrip.PerformLayout();
+			this.statusStrip.ResumeLayout(false);
+			this.statusStrip.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ToolStrip toolStrip;
+		private System.Windows.Forms.ToolStripDropDownButton fileMenu;
+		private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+		private System.Windows.Forms.ToolStripButton runButton;
+		private System.Windows.Forms.StatusStrip statusStrip;
+		private System.Windows.Forms.ToolStripStatusLabel lineLabel;
+		private System.Windows.Forms.ToolStripStatusLabel columnLabel;
+		private System.Windows.Forms.ToolStripStatusLabel fileNameLabel;
+	}
+}
+
