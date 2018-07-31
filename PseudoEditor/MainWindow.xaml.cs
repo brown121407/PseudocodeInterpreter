06506e429062d4c298fe0302f71b236d74165216
Sergiu Marton <alexander.sergiu@gmail.com>
2018-07-31 14:22:47 +0300

    Implemented basic editor functionality
    Opening, creating, saving and editing files

--- PseudoEditor/MainWindow.xaml.cs
+++ PseudoEditor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using Path = System.IO.Path;
 
 namespace PseudoEditor
 {
@@ -20,34 +23,80 @@ namespace PseudoEditor
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private string _currentFileName;
+
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
 
-		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			
-		}
-
 		private void New_Executed(object sender, ExecutedRoutedEventArgs e)
 		{
-			
+			_currentFileName = null;
+			var saveFileDialog = new SaveFileDialog
+			{
+				CheckPathExists = true,
+				DefaultExt = "pseudo",
+				FileName = "NewFile",
+				OverwritePrompt = true,
+				Filter = "Pseudocode file (*.pseudo)|*.pseudo|Text file (*.txt)|*.txt|All files (*.*)|*.*"
+			};
+
+			if (saveFileDialog.ShowDialog() == true)
+			{
+				_currentFileName = saveFileDialog.FileName;
+				File.Create(_currentFileName).Close();
+				Editor.Load(_currentFileName);
+			}
 		}
 
-		private void SaveAs_Executed(object sender, ExecutedRoutedEventArgs e)
+		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
 		{
-			
+			var dialog = new OpenFileDialog {
+				CheckFileExists = true,
+				Filter = "Pseudocode file (*.pseudo)|*.pseudo|Text file (*.txt)|*.txt|All files (*.*)|*.*"
+			};
+
+			if (dialog.ShowDialog() == true)
+			{
+				_currentFileName = dialog.FileName;
+				Editor.Load(_currentFileName);
+			}
 		}
 
 		private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
 		{
-			
+			if (_currentFileName == null)
+			{
+				SaveAs_Executed(sender, e);
+			}
+			else
+			{
+				Editor.Save(_currentFileName);
+			}
+		}
+
+		private void SaveAs_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			var dialog = new SaveFileDialog
+			{
+				FileName = (_currentFileName == null ? "NewFile" : Path.GetFileName(_currentFileName)),
+				DefaultExt = "pseudo",
+				OverwritePrompt = true,
+				CheckPathExists = true,
+				Filter = "Pseudocode file (*.pseudo)|*.pseudo|Text file (*.txt)|*.txt|All files (*.*)|*.*"
+			};
+
+			if (dialog.ShowDialog() == true)
+			{
+				_currentFileName = dialog.FileName;
+				Editor.Save(_currentFileName);
+			}
 		}
 
 		private void Close_Executed(object sender, ExecutedRoutedEventArgs e)
 		{
-			
+			this.Close();
 		}
 	}
 }
---
+++ PseudoEditor/StringResources.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PseudoEditor
+{
+	public static class StringResources
+	{
+		public static string New { get; } = "Nou";
+
+		public static string NewFile { get; } = "Fișier nou";
+
+		public static string Cut { get; } = "Taie";
+
+		public static string Copy { get; } = "Copiază";
+
+		public static string Paste { get; } = "Lipește";
+
+		public static string Save { get; } = "Salvează";
+		
+		public static string SaveAs { get; } = "Salvează ca";
+
+		public static string Open { get; } = "Deschide";
+
+		public static string Close { get; } = "Închide";
+
+		public static string Execute { get; } = "Execută";
+	}
+}
