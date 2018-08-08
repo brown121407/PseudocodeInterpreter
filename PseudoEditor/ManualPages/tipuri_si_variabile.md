5a564909b8c0e8075e0d98486917abc28645b28a
Sergiu Marton <alexander.sergiu@gmail.com>
2018-08-08 07:42:05 +0300

    Added hyperlink support on manual pages

---
+++ PseudoEditor/ManualPages/tipuri_si_variabile.md
@@ -0,0 +1,11 @@
+# Tipuri si variabile
+
+Intr-un program, pentru a memora valori se folosesc **variabile**. **Variabilele** pot fi vazute ca niste cutii cu etichete.
+ Numele variabilei este eticheta, iar valoarea ei este ceea ce se afla in interiorul cutiei.  
+De exemplu, pentru a tine minte ca avem 30 de lei putem scrie:  
+```intreg sumaDeBani <- 30```  
+In acest exemplu, `sumaDeBani` este o **variabila**, `30` este valoarea pe care o memoreaza, iar `intreg` este **tipul** variabilei.  
+Variabilele trebuie sa aiba un tip anume pentru a stoca diferite valori, cum ar fi:
+- `intreg`, pentru a memora numere intregi (3, 2234, -56)
+- `real`, pentru a memora numere reale (3.14, -95.44375)
+- `text`, pentru a memora siruri de caractere ("sunt foarte bun la programare", "sunt si foarte modest")
--- PseudoEditor/ManualWindow.xaml.cs
+++ PseudoEditor/ManualWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using Markdig;
 using Markdig.Wpf;
 
@@ -16,18 +19,19 @@ namespace PseudoEditor
 	{
 		private readonly string _manualPagesPath = $"{Directory.GetCurrentDirectory()}/ManualPages/";
 		private const string MarkdownExt = ".md";
+		private const string ManualUriScheme = "manual";
 
 		private const string PageMissingError = "# Pagina lipsa din manual\n" +
 		                                        "Pagina nu exista sau lipseste din folderul ManualPages. " +
 		                                        "Paginile manualului pot fi descarcate de pe GitHub.";
 
-		private List<string> _fileNames = new List<string>
+		private readonly List<string> _fileNames = new List<string>
 		{
-			"Cuprins",
-			"Ghidul editorului",
-			"Tipuri si variabile",
-			"Instructiuni",
-			"Algoritmi de baza"
+			"cuprins",
+			"ghidul_editorului",
+			"tipuri_si_variabile",
+			"instructiuni",
+			"algoritmi"
 		};
 
 		public ManualWindow()
@@ -37,6 +41,19 @@ namespace PseudoEditor
 			Viewer.Pipeline = new MarkdownPipelineBuilder().UseSupportedExtensions().Build();
 
 			LoadButtonsForPages();
+
+			Viewer.Loaded += async (sender, args) =>
+			{
+				var firstFilePath = Path.Combine(_manualPagesPath, $"{_fileNames[0]}{MarkdownExt}");
+				if (File.Exists(firstFilePath))
+				{
+					Viewer.Markdown = await File.OpenText(firstFilePath).ReadToEndAsync();
+				}
+				else
+				{
+					Viewer.Markdown = PageMissingError;
+				}
+			};
 		}
 
 		private void LoadButtonsForPages()
@@ -46,7 +63,10 @@ namespace PseudoEditor
 				foreach (var fileName in _fileNames)
 				{
 					var filePath = Path.Combine(_manualPagesPath, $"{fileName}{MarkdownExt}");
-					CreateButton(fileName, File.Exists(filePath) ? filePath : null);
+
+					var buttonContent = fileName.Replace('_', ' ').ToUpper();
+
+					CreateButton(buttonContent, File.Exists(filePath) ? filePath : null);
 				}
 			}
 		}
@@ -69,6 +89,27 @@ namespace PseudoEditor
 			}
 
 			PanelButtons.Children.Add(button);
+		}
+
+		private async void Hyperlink_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			var uri = new Uri(e.Parameter.ToString());
+			if (uri.Scheme == ManualUriScheme)
+			{
+				var filePath = Path.Combine(_manualPagesPath, uri.Host);
+				if (File.Exists(filePath))
+				{
+					Viewer.Markdown = await File.OpenText(filePath).ReadToEndAsync();
+				}
+				else
+				{
+					Viewer.Markdown = PageMissingError;
+				}
+			}
+			else
+			{
+				Process.Start(e.Parameter.ToString());
+			}
 		}
 	}
 }
